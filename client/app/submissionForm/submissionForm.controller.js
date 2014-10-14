ebdd21e049357b6e081493b9be69147cc1752df3
lehet005 <lehet005@morris.umn.edu>
2014-10-13 20:54:26 -0500

    started to add db functionallity

--- client/app/submissionForm/submissionForm.controller.js
+++ client/app/submissionForm/submissionForm.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ursamajorApp')
-  .controller('SubmissionformCtrl', function ($scope) {
+  .controller('SubmissionformCtrl', function ($scope, $http) {
 
         $scope.format = [
             '',
@@ -35,6 +35,32 @@ angular.module('ursamajorApp')
         ];
 
         $scope.submitSubmission = function() {
-
+        $http.post('/api/submissions', {
+            titleName: $scope.title,
+            format: $scope.format,
+            abstract: $scope.abstract,
+            typeofPres: $scope.type,
+            flexibleType: $scope.space,
+            lastName: $scope.studentLast,
+            firstName: $scope.studentFirst,
+            lastNameCo: $scope.studentCoLast,
+            firstNameCo: $scope.studentCoFirst,
+            lastNameCo2: $scope.studentCo2Last,
+            firstNameCo2: $scope.studentCo2First,
+            studentEmail: $scope.studentEmail,
+            studentCoEmail: $scope.studentCoEmail,
+            studentCo2Email: $scope.studentCo2Email,
+            discipline: $scope.discipline,
+            //sponsor: $scope.,
+            adviser: $scope.advisor,
+            adviserEmail: $scope.advisorEmail,
+            feature: $scope.feature,
+            mediaNeeds: $scope.mediaNeeds,
+            otherNeeds: $scope.otherNeeds,
+            tshirt: $scope.sizes
+            //tshirtCo: $scope.,
+            //tshirtCo2: String,
+            //otherInfo: ,
+        }).success(function())
         }
   });
